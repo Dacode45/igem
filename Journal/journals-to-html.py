dd7711035541c04221af26febbc215cb33ae1e05
David Ayeke <ayekedavid@yahoo.com>
2015-07-31 13:50:32 -0500

    Updated the description as well as other minor changes

--- Journal/journals-to-html.py
+++ Journal/journals-to-html.py
@@ -18,11 +18,31 @@ def parseData(filenames):
             data[filename[:-5]] = json.load(data_file)
     return data
 
-def addScaffold()
+def addScaffold(html, data):
+    tabDiv = html.new_tag("div", id="journal_tabs")
+    tabList = tabDiv.new_tag("ul", class="nav nav-tabs" role="tablist")
+    tabContent = tabDiv.new_tag("div" class="tab-content")
+
+    for person, journal in data.iteritems():
+        #Actual tabs
+        li = tabList.new_tag("li", role="presentation")
+        link = li.new_tag("a", href=person+"_content" aria-controls=person+"_content", role="tab", data-toggle="tab")
+        li.append(link)
+        tabList.append(li)
+
+        #tab content
+        tabPanel = tabContent.new_tag("div", role="tabpanel", class="tab-pane", id=person+"_content")
+        tabContent.append(tabPanel)
+
+    tabDiv.append(tabList)
+    tabDiv.append(tabContent)
+    html.append(tabDiv)
+
 
 def dataToHTML(data, html):
     container = html.find(id="journal_container")
-    addScaffold(container)
+    addScaffold(container, data)
+
     for person, journal in data.iteritems():
         personDiv = html.new_tag("div")
         personDiv.string = person
